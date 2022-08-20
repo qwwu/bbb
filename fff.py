259228fb10abeee999efb39c26c40dc4363f11a4
qwwu <huajun87366>
2022-08-20 21:14:44 +0800

    修改之后提交

--- fff.py
+++ fff.py
@@ -6,6 +6,10 @@ def func():
         print('c', c)
     else:
         print('a', a)
+# 修改代码之后代码颜色是绿色部分
+new_list = ['python', 'java', 'c+']
+for n in new_list:
+    print(n)
 
 if __name__ == '__main__':
     func()
