ed146870334765c0b9212b38ad59bce838cf88a2
qwwu <huajun87366>
2022-08-20 20:46:41 +0800

    修改之后提交

--- one.py
+++ one.py
@@ -1,1 +1,11 @@
-# 定义一个函数
+# 定义一个函数   文件未提交的话是橘色，提交之后是黑色，修改之后是蓝色
+def  comm():
+    a = 1
+    b = 2
+    c = a + b
+    return c
+
+if __name__ == '__main__':
+    rs = comm()
+    print(rs)
+
--- show/comm.py
+++ show/comm.py
@@ -1,5 +1,11 @@
 # 新建一个列表
-new_list = ['python', 'java', 'c++']
-for i in new_list:
-    print(i)
+if __name__ == '__main__':
+
+    new_list = ['python', 'java', 'c++']
+    for i in new_list:
+        print(i)
+
+    for k in range(1, 11):
+        print(k)
+
 
